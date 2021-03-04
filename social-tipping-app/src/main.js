ca6c348d9553d14d8259f20ed6cbdad7300f2d56
kawasaki <k.kawasaki0820@gmail.com>
2021-03-04 18:38:55 +0900

    ユーザ一覧表示機能の作成

--- social-tipping-app/src/main.js
+++ social-tipping-app/src/main.js
@@ -7,10 +7,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
   store,
-  BootstrapVue,
   render: h => h(App)
 }).$mount('#app')
